d85c795bf90db3ad0ce6f8d6221376ad2f424b90
angarita <44899916+codingAngarita@users.noreply.github.com>
2020-06-23 17:15:19 -0500

    Added array analysis tests

--- array-analysis.js
+++ array-analysis.js
@@ -1,10 +1,11 @@
 function arrayAnalyze(array) {
+  if(typeof array != 'object') throw new TypeError;
   const length = array.length;
   let sum = 0;
   let min, max;
 
   array.forEach( number => {
-    if(typeof number != 'number') throw TypeError;
+    if(typeof number != 'number') throw new TypeError;
     if(min === undefined || number < min) min = number;
     if(max === undefined || number > max) max = number;
     sum += number;
